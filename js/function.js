82d312059a9ecffbd9813af6ac1c7f92482ce1ba
unknown <sakib21feb@gmail.comgit config --global user.name>
2024-09-24 18:57:35 +0600

    added two js files and edited some html files

---
+++ js/function.js
@@ -0,0 +1,28 @@
+function fieldValueById(id) {
+    const inputValue = document.getElementById(id).innerText
+    const inputNumber = parseFloat(inputValue)
+    return inputNumber
+}
+
+
+function inputFieldValueById(id) {
+    const inputValue = document.getElementById(id).value
+
+    digits = '0123456789.'
+    let decimalCount = 0
+    for (let i = 0; i <inputValue.length; i++) {
+        if (!digits.includes(inputValue[i])) {
+            return NaN
+        }
+
+        if (inputValue[i] === '.') {
+            decimalCount++
+        }
+        
+        if (decimalCount > 1) {
+            return NaN
+        }
+    }
+    let inputNumber = Math.floor(inputValue)
+    return inputNumber
+}
---
+++ js/script.js
@@ -0,0 +1,118 @@
+// Working on Donate Button-------------------------------------------
+
+const donation = document.getElementById('donation');
+
+function handleButtonColorOnPageLoad() {
+    const currentPage = document.location.pathname;
+    
+    if (currentPage === '/html/index.html' || currentPage === '/') {
+        donation.style.backgroundColor = '#B4F461';
+    } else {
+        donation.style.backgroundColor = '';
+    }
+}
+handleButtonColorOnPageLoad();
+
+donation.addEventListener('click', function() {
+    donation.style.backgroundColor = '#B4F461';
+});
+
+// Working on History Button---------------------------------------------
+
+
+
+// Donate for Flood at Noakhali-------------------------------------------
+
+document.getElementById('donateButtonNoakhali')
+.addEventListener('click', function(event1) {
+    event1.preventDefault()
+
+    const donateToNoakhali = inputFieldValueById('donateToNoakhali')
+    let totalFundNoakhali = fieldValueById('totalFundNoakhali')
+    let balance = fieldValueById('balance')
+    const modal = document.getElementById('my_modal_1')
+
+    if (isNaN(donateToNoakhali)){
+        alert('Failed!')
+        return
+    }
+    if (donateToNoakhali > balance || donateToNoakhali <= 0) {
+        alert('Failed')
+        return
+    }
+
+    const newBalance = balance - donateToNoakhali;
+    totalFundNoakhali += donateToNoakhali;
+    
+    document.getElementById('balance').innerText = newBalance
+    document.getElementById('totalFundNoakhali').innerText = totalFundNoakhali
+
+    modal.showModal()
+
+    document.getElementById('donateToNoakhali').value = ''
+})
+
+// Donate for Flood at Feni-------------------------------------------------- 
+
+document.getElementById('donateButtonFeni')
+.addEventListener('click', function(event2) {
+    event2.preventDefault()
+
+    const donateToFeni = inputFieldValueById('donateToFeni')
+    let totalFundFeni = fieldValueById('totalFundFeni')
+    let balance = fieldValueById('balance')
+    const modal = document.getElementById('my_modal_1')
+
+    if (isNaN(donateToFeni)){
+        alert('Failed!')
+        return
+    }
+    if (donateToFeni > balance || donateToFeni <= 0) {
+        alert('Failed')
+        return
+    }
+
+    const newBalance = balance - donateToFeni;
+    totalFundFeni += donateToFeni;
+    
+    document.getElementById('balance').innerText = newBalance
+    document.getElementById('totalFundFeni').innerText = totalFundFeni
+
+    modal.showModal()
+
+    document.getElementById('donateToFeni').value = ''
+})
+
+
+
+// Aid for Injured in the Quota Movement-------------------------------------------------- 
+
+document.getElementById('donateButtonQuotaMovement')
+.addEventListener('click', function(event2) {
+    event2.preventDefault()
+
+    const donateToQuotaMovement = inputFieldValueById('donateToQuotaMovement')
+    let totalFundQuotaMovement = fieldValueById('totalFundQuotaMovement')
+    let balance = fieldValueById('balance')
+    const modal = document.getElementById('my_modal_1')
+
+    if (isNaN(donateToQuotaMovement)){
+        alert('Failed!')
+        return
+    }
+    if (donateToQuotaMovement > balance || donateToQuotaMovement <= 0) {
+        alert('Failed')
+        return
+    }
+
+    const newBalance = balance - donateToQuotaMovement;
+    totalFundQuotaMovement += donateToQuotaMovement;
+    
+    document.getElementById('balance').innerText = newBalance
+    document.getElementById('totalFundQuotaMovement').innerText = totalFundQuotaMovement
+
+    modal.showModal()
+    
+    document.getElementById('donateToQuotaMovement').value = ''
+})
+
